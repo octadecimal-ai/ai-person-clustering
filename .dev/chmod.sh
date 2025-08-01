f0629fa73c6f73441423ae2e0f7abf146b5bb5bd
piotradamczyk8 <piotr.k.adamczyk@gmail.com>
2025-08-02 00:04:07 +0200

    Migracja repo do git@github.com:octadecimal-ai/ai-person-clustering.git

--- .dev/chmod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Nadaje chmod +x wszystkim plikom (nie katalogom) w bieżącym katalogu
-for file in *; do
-  if [ -f "$file" ]; then
-    chmod +x "$file"
-    echo "Dodano +x dla: $file"
-  fi
-done
--- .dev/time.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Wyświetlenie aktualnej daty i godziny w formacie YYYY-MM-DD H:i:s
-date "+%Y-%m-%d %H:%M:%S"
